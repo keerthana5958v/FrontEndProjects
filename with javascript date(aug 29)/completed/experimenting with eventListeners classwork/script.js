942767f09b526f060436d1a2af0c09b26b6b53a1
keerthana5958v <127711144+keerthana5958v@users.noreply.github.com>
2024-03-25 17:10:19 +0530

    Delete with javascript date(aug 29) directory

--- with javascript date(aug 29)/completed/experimenting with eventListeners classwork/script.js
+++
@@ -1,6 +0,0 @@
-const X = document.getElementById('div');
-document.body.addEventListener('mousedown', function(event){
-    console.log(event);
-    X.style.top = event.clientY+"px";
-    X.style.left = event.clientX+"px";
-})
